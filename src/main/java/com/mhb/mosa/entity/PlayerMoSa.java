26c6555c3f597c4c88fb53102d2bd49088c943e6
maohangbin <271221198@qq.com>
2021-06-21 18:15:59 +0800

    创建、加入、离开、关闭房间

--- src/main/java/com/mhb/mosa/entity/PlayerMoSa.java
+++ src/main/java/com/mhb/mosa/entity/PlayerMoSa.java
@@ -1,14 +1,17 @@
 package com.mhb.mosa.entity;
 
+import com.alibaba.fastjson.JSON;
 import com.mhb.mosa.memory.RoleHomeEnum;
 import com.mhb.mosa.memory.SessionHome;
+import com.mhb.mosa.service.impl.MosaServiceImpl;
+import com.mhb.mosa.service.impl.PlayerServiceImpl;
 import com.mhb.mosa.util.StaticUtils;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.springframework.util.CollectionUtils;
+import org.apache.commons.collections.CollectionUtils;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 /**
  * @date: 2021/6/2 17:44
@@ -29,28 +32,62 @@ public class PlayerMosa extends Player implements Comparable {
 
     public PlayerMosa(String sessionId, String userName) {
         super(sessionId, userName, RoleHomeEnum.MOSA);
-    }
-
-    @Override
-    public void afterConnectionEstablished() {
-        String[] fields = {"roomId", "index"};
-        List<String> list = StaticUtils.playerService.getPlayer(getUserName(), fields);
+        List<String> list = StaticUtils.playerService.getProperties(getUserName(), "roomId", "index");
         if (!CollectionUtils.isEmpty(list) && list.size() > 1) {
             this.roomId = list.get(0);
             this.index = Integer.parseInt(list.get(1));
         }
-        //TODO 通知房间内玩家信息
+    }
+
+    @Override
+    public void afterConnectionEstablished() {
+        //缓存玩家信息
+        Map<String, String> playerMap = new HashMap<>(2);
+        playerMap.put("sessionId", getSessionId());
+        playerMap.put("userName", getUserName());
+        StaticUtils.jedisCluster.hmset(PlayerServiceImpl.redisKeyHashPlayerInfo(getUserName()), playerMap);
+        //发送信息
+        try {
+            sendAll(JSON.toJSONString(new TextMsg<>(TextMsgEnum.MOSA.getModule(), TextMsgEnumMosa.JOIN_ROOM.getType(), getUserName() + "加入房间")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         super.afterConnectionEstablished();
     }
 
     @Override
     public void afterConnectionClosed() {
+        StaticUtils.mosaService.leaveRoom(this.roomId, getUserName(), String.valueOf(this.index));
+        if (StaticUtils.jedisCluster.hlen(MosaServiceImpl.redisKeyHashRoomPlayerInfo(this.roomId)) == 0) {
+            StaticUtils.mosaService.closeRoom(this.roomId);
+        }
         try {
-            SessionHome.sendMsg(getSessionId(), "连接丢失");
+            sendOther(JSON.toJSONString(new TextMsg<>(TextMsgEnum.MOSA.getModule(), TextMsgEnumMosa.LEAVE_ROOM.getType(), getUserName() + "离开房间")));
         } catch (IOException e) {
             e.printStackTrace();
         }
         super.afterConnectionClosed();
+    }
+
+    @Override
+    public void sendAll(String msg) throws IOException {
+        List<String> listUserName = StaticUtils.mosaService.listUserNameInRoom(this.roomId);
+        List<String> listSessionId = new ArrayList<>();
+        for (String username : listUserName) {
+            listSessionId.add(StaticUtils.playerService.getProperties(username, "sessionId").get(0));
+        }
+        SessionHome.sendMsg(listSessionId, msg);
+    }
+
+    @Override
+    public void sendOther(String msg) throws IOException {
+        List<String> listUserName = StaticUtils.mosaService.listUserNameInRoom(this.roomId);
+        listUserName.remove(getUserName());
+        List<String> listSessionId = new ArrayList<>();
+        for (String username : listUserName) {
+            listSessionId.add(StaticUtils.playerService.getProperties(username, "sessionId").get(0));
+        }
+        SessionHome.sendMsg(listSessionId, msg);
     }
 
     @Override
